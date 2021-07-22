9a3e715cd189c3006b8e4d1fd74461c8fef651b6
ayadi mohammed <ayadi.mohamed@outlook.com>
2021-07-22 16:11:07 +0100

    creating subdomain-shodan-finder.py

---
+++ subdomain-shodan-finder.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" 
+    Author : ayadi mohammed
+    Github account : ayadim
+    script title : subdomain-shodan-finder
+    discription : this script get DNS records collected from shodan search engine then we can extract subdomains
+
+
+    How it works 
+    1- you should have an API-key for shodan
+    2- run it : python3 subdomain-shodan-finder.py domain.com
+
+ """
+import requests,json,sys
+
+shodan_apikey = "" #change it 
+domain = sys.argv[1]
+listOfSubdomains={"www"}
+link ="https://api.shodan.io/dns/domain/"+domain+"?key="+shodan_apikey
+
+#Request Shodan for data
+response = requests.get(link)
+data = response.json()
+for item in data["data"]:
+    listOfSubdomains.add(item["subdomain"])
+
+for item in listOfSubdomains:
+    print(item+"."+domain)
