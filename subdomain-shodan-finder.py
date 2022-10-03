6f40e64f796740a9bd52c660fb8a01ed1c466d1c
ayadim <ayadim@email.com>
2022-10-03 20:52:22 +0100

    improve the script

--- subdomain-shodan-finder.py
+++ subdomain-shodan-finder.py
@@ -11,7 +11,15 @@
     2- run it : python3 subdomain-shodan-finder.py domain.com
 
  """
+
 import requests,json,sys
+
+#check command line input
+if len(sys.argv) != 2:
+    print("Warning : You should add a domain")
+    print("Example : python3 subdomain-shodan-finder.py")
+    exit()
+
 
 shodan_apikey = "" #change it 
 domain = sys.argv[1]
@@ -19,10 +27,22 @@ listOfSubdomains={"www"}
 link ="https://api.shodan.io/dns/domain/"+domain+"?key="+shodan_apikey
 
 #Request Shodan for data
-response = requests.get(link)
-data = response.json()
+response = ""
+data = ""
+try:
+    response = requests.get(link)
+    data = response.json()
+    response = requests.get(link)
+    data = response.json()
+except Exception:
+    print("Check that your api key is valid.")
+    exit()
+
+
+
 for item in data["data"]:
     listOfSubdomains.add(item["subdomain"])
 
 for item in listOfSubdomains:
-    print(item+"."+domain)
+    if item != "":
+        print(item+"."+domain)
